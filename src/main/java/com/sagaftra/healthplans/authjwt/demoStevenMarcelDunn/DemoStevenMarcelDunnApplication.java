776cbbfa06113b102de15b3e7ce9a6b48bc0d9aa
sdunnvo <124534566+sdunnvo@users.noreply.github.com>
2025-03-11 20:45:19 -0700

    Initial files introduced
    Let's begin Authentication & Authorization with JWT

---
+++ src/main/java/com/sagaftra/healthplans/authjwt/demoStevenMarcelDunn/DemoStevenMarcelDunnApplication.java
@@ -0,0 +1,13 @@
+package com.sagaftra.healthplans.authjwt.demoStevenMarcelDunn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStevenMarcelDunnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStevenMarcelDunnApplication.class, args);
+	}
+
+}
